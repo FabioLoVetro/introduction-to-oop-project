78dab1662f213307a22afc98402729c99cce7de6
Fabio <fabio.lovetro@gmail.com>
2022-09-03 20:15:00 +0100

    Add project files.

---
+++ Exercises/Pigeon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduction_to_oop_project.Exercises
+{
+    internal class Pigeon
+    {
+    }
+}
