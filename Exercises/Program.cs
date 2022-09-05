0fa0b13b1702c1f24714a3f6f39614910f263ceb
Fabio <fabio.lovetro@gmail.com>
2022-09-05 12:58:59 +0100

    instance of bird removed

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,10 +1,6 @@
 ﻿
 using introduction_to_oop_project.Exercises;
 
-Bird myBird = new Bird("Tweety", "Yellow");
-myBird.Speak();
-myBird.Fly();
-
 Pigeon pippa = new Pigeon("Pippa", "Grey");
 pippa.Speak();
 pippa.Fly();
