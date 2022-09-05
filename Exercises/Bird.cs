0bd6eda80903e85cf2fa18220ae2955ece8323f5
Fabio <fabio.lovetro@gmail.com>
2022-09-05 12:50:43 +0100

    abstract bird

--- Exercises/Bird.cs
+++ Exercises/Bird.cs
@@ -2,7 +2,7 @@
 
 namespace introduction_to_oop_project.Exercises
 {
-    public class Bird
+    public abstract class Bird
     {
         public string Name { get; private set; }
         public string Color { get; private set; }
@@ -13,13 +13,15 @@ namespace introduction_to_oop_project.Exercises
             Color = color;
         }
 
-        public void Speak()
+        public virtual void Speak()
         {
+            //default implementation
             Console.WriteLine($"My name is {Name} and I am a {Color} bird.");
         }
 
-        public void Fly()
+        public virtual void Fly()
         {
+            //default implementation
             Console.WriteLine($"I'm {Name} and I can fly high in the blue sky!");
         }
     }
